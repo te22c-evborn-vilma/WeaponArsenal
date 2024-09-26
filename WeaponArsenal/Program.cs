f647af65eec2bd98a7dd491a30ec55b40342ef2e
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-09-26 10:59:45 +0200

    Commit

--- WeaponArsenal/Program.cs
+++ WeaponArsenal/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Weapon firstWeapon = new Weapon() 
+{
+    Name = "Katana",
+    DamageMin = 4,
+    DamageMax = 19
+};
---
+++ WeaponArsenal/Weapon.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+public class Weapon
+{
+    [JsonInclude]
+    public string Name;
+
+    [JsonInclude]
+    public int DamageMin;
+
+    [JsonInclude]
+    public int DamageMax;
+
+    public void Attack(int dmg)
+    {
+        
+    }
+}
