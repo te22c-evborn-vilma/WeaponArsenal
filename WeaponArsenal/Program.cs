28eb86402a75be1cf942cb2cac6f4745a9f64cc7
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-10-10 10:53:00 +0200

    Choose weapon

--- WeaponArsenal/Program.cs
+++ WeaponArsenal/Program.cs
@@ -2,17 +2,26 @@
 using System.Net.Mail;
 using System.Text.Json.Serialization; 
 
-
 string textEnemy = File.ReadAllText("enemy.json");
 List<Enemy> enemies = JsonSerializer.Deserialize<List<Enemy>>(textEnemy);
 
 string textWeapon = File.ReadAllText("weapons.json");
 List<Weapon> weapons = JsonSerializer.Deserialize<List<Weapon>>(textWeapon);
 
-// Console.WriteLine(weapons[0].Name);
-// Console.WriteLine(enemies[0].Name);
+Console.WriteLine("Please choose a weapon (only write the number before the name)\n");
 
-Console.WriteLine("How many attacks do you want to make?");
+int length = weapons.Count;
+for (int j = 0; j < length; j++)
+{
+    Console.WriteLine($"{j}. Name: {weapons[j].Name} || Minimum Damage: {weapons[j].DamageMin} || Maximum Damage: {weapons[j].DamageMax}\n");
+}
+
+string choice = Console.ReadLine();
+
+int weaponChoice = 0;
+int.TryParse(choice, out weaponChoice);
+
+Console.WriteLine("How many attacks do you want to make? (only write the number of attacks)");
 
 string atkNumAnswer = Console.ReadLine();
 
@@ -20,24 +29,25 @@ int atkNum = 0;
 
 int.TryParse(atkNumAnswer, out atkNum);
 
-
-// int totDmg = 0;
-
 for (int i = 0; i < atkNum; i++)
 {
-    int dmg = weapons[0].Attack();    
+    int dmg = weapons[weaponChoice].Attack();    
     Console.WriteLine($"{dmg}");
 
     enemies[0].HitPoints -= dmg;
 }
 
-Console.WriteLine($"{enemies[0].Name} has {enemies[0].HitPoints} Hp left");
-
-// Console.WriteLine($"The total damage was {totDmg}");
+Console.WriteLine($"{enemies[0].Name} was hit {atkNum} number of time(s) and has {enemies[0].HitPoints} Hp left");
 
 Console.ReadLine();
 
 
+
+
+// Console.WriteLine(weapons[0].Name);
+// Console.WriteLine(enemies[0].Name);
+// int totDmg = 0;
+// Console.WriteLine($"The total damage was {totDmg}");
 // Enemy firstEnemy = new Enemy()
 // {
 //     Name = "Bobo",
